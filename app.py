5a6e900b49f1ca67abbf0c94889df1302506de9a
Strelizia53 <strelizia0530@gmail.com>
2025-06-16 14:08:45 +0530

    fixed CORS

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import cv2
-# from pyzbar.pyzbar import decode
 from PIL import Image
 import os, tempfile
 
 app = Flask(__name__)
+CORS(app)  # ✅ Enable CORS for all routes
 
 def check_qr_damage(img_path):
-    import cv2
     img = cv2.imread(img_path)
     detector = cv2.QRCodeDetector()
     data, points, _ = detector.detectAndDecode(img)
     return "undamageQR" if data else "damageQR"
-
 
 @app.route("/scan", methods=["POST"])
 def scan_qr():
